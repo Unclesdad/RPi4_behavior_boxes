3f9df92b2ee0db2435fe7db1bc9bd907c940cd80
Unclesdad <basaltjazz3@gmail.com>
2025-07-17 11:53:41 -0400

    wasnt keeping main thread alive my bad

--- video_acquisition/test_irig_h.py
+++ video_acquisition/test_irig_h.py
@@ -1,9 +1,12 @@
 import irig_h_gpio as irig
+import time
 
 sender = irig.IrigHSender(sending_gpio_pin=6)
 
 try:
     sender.start()
+    while True:
+        time.sleep(1)
 except KeyboardInterrupt:
     print('keyboard interrupt recieved. stopping...')
 finally:
